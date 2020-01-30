2b177a31ed9d07e27d4d1f8b7db41b2a77432be4
GauravXd <grvdhingra1999@gmail.com>
2020-01-30 15:34:43 +0530

    Weather++

--- Lecture6/axios.js
+++ Lecture6/axios.js
@@ -2,16 +2,33 @@
 const axios = require("axios");
 
 async function helper() {
-    const response = await axios.get(
-        "https://www.metaweather.com/api/location/search/?query=Boston"
-    );
+    const date = process.argv[2];
+    const cityName = process.argv[3];
+    try {
+        const response = await axios.get(
+            "https://www.metaweather.com/api/location/search/?query=" + cityName
+        );
+    } catch (err) {
+        console.log("False Value")
+        console.log(err)
+    };
     // you will be inside data key
-    console.log(response.data[0].woeid);
+    // console.log(response.data[0].woeid);
     var woeid = response.data[0].woeid;
 
     const AnotherResponse = await axios.get(
         "https://www.metaweather.com/api/location/" + woeid + "/"
     );
-    console.log(AnotherResponse.data);
+    var i = 0
+    if (date == "today") {
+        i = 0
+    } else if (date == "tomorrow") {
+        i = 1
+    }
+    console.log("Date: " + AnotherResponse.data.consolidated_weather[i].applicable_date + "\n" +
+        "Weather: " + AnotherResponse.data.consolidated_weather[i].weather_state_name +
+        "\n" + "Min Temp: " + AnotherResponse.data.consolidated_weather[i].min_temp.toFixed(2) +
+        "\n" + "Max Temp: " + AnotherResponse.data.consolidated_weather[i].max_temp.toFixed(2) + "\n"
+        + "Humidity: " + AnotherResponse.data.consolidated_weather[i].humidity);
 }
 helper();
